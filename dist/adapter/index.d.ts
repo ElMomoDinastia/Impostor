9c0e4037514e35b60a81ee387d21826bc3d24849
ElMomoDinastia <84684306+ElMomoDinastia@users.noreply.github.com>
2025-12-31 14:05:12 -0300

    Delete dist/adapter/index.d.ts

--- dist/adapter/index.d.ts
+++
@@ -1,4 +0,0 @@
-export { IHBRoomAdapter, HBPlayer, RoomConfig, RoomEventHandlers, AnnouncementOptions } from './types';
-export { HBRoomAdapter, createHBRoomAdapter } from './haxball.adapter';
-export { MockHBRoomAdapter, createMockAdapter } from './mock.adapter';
-//# sourceMappingURL=index.d.ts.map
